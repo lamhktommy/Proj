a7edcb083ad774bb2f3cedbb4ee160dccef53d2f
lamhktommy <35717697+lamhktommy@users.noreply.github.com>
2018-05-27 16:01:50 +0800

    Delete VolumeDownModule.py

--- module/VolumeDownModule.py
+++
@@ -1,9 +0,0 @@
-moduleName = "VolumeDownModule"
-
-#All of the words must be heard in order for this module to be executed
-commandWords = ["volume","down"]
-
-def execute(command):
-    #Write anything you want to be executed when the commandWords are heard
-    #The 'command' parameter is the command you speak
-    print("Volume down by 5")
