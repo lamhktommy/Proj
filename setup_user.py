b03cc973fab330a61d62a01100e072644d74c1e8
lamhktommy <35717697+lamhktommy@users.noreply.github.com>
2018-05-27 15:46:50 +0800

    Delete setup_user.py

--- setup_user.py
+++
@@ -1,7 +0,0 @@
-print("------------------------------------------------------------------------")
-print("|                            New User Setup                            |")
-print("|                  Thank you for choosing Potato Inc.                  |")
-print("------------------------------------------------------------------------")
-name=input("Input your name: ")
-channel= input("Favourite channel: ")
-
